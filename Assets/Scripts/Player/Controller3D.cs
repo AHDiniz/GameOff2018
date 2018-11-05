2e1c0dbfc983dd83c921ec7e70ad494939f546aa
AHDiniz <AHDiniz@github.com>
2018-11-04 22:10:18 -0200

    Starting to make 3D animations

--- Assets/Scripts/Player/Controller3D.cs
+++ Assets/Scripts/Player/Controller3D.cs
@@ -10,18 +10,22 @@ namespace GameOff2018
 		{
 			[SerializeField] private float walkSpeed;
 			[SerializeField] private float runSpeed;
+			[SerializeField] private float strafeSpeed;
 			[SerializeField] private float rotationSpeed;
 
 			private Rigidbody rBody;
+			private Animator animator;
 
 			void Start()
 			{
 				rBody = GetComponent<Rigidbody>();
+				animator = GetComponent<Animator>();
 			}
 			
 			void FixedUpdate()
 			{
 				ForwardMovement();
+				Strafe();
 				Rotation();
 			}
 
@@ -30,6 +34,12 @@ namespace GameOff2018
 				float input = Input.GetAxis("Vertical");
 				float speed = (Input.GetButton("Fire3")) ? runSpeed : walkSpeed;
 				rBody.AddRelativeForce(Vector3.forward * input * speed, ForceMode.Force);
+			}
+
+			private void Strafe()
+			{
+				float input = Input.GetAxis("Strafe");
+                rBody.AddRelativeForce(Vector3.right * input * strafeSpeed, ForceMode.Force);
 			}
 
 			private void Rotation()
